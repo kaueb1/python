0ea0964def984c100a67e278a1817eecbdb30c11
kaueb1 <kauebvieira@gmail.com>
2025-06-30 16:23:21 -0300

    Aula 8 – Utilizando Módulos

---
+++ aula08a.py
@@ -0,0 +1,4 @@
+from math import sqrt, floor
+num = int(input('Digíte um número: '))
+raiz = sqrt(num)
+raiz = print('A raiz de {} é igual a {}'.format(num, raiz))
--- ex011.py
+++ ex011.py
@@ -1,6 +1,6 @@
-larg = float(input("Largura da parede: "))
-alt = float(input("Altura da parede: "))
+larg = float(input('Largura da parede: '))
+alt = float(input('Altura da parede: '))
 área = larg * alt
-print("Sua parede tem a dimensão de {}x{} e sua área é de {} metros quadrados.".format(larg, alt, área))
+print("Sua parede tem a dimensão de {} x {} e sua área é de {} metros quadrados.".format(larg, alt, área))
 tinta = área / 2
-print('Para pintar essa pareded, você precisará de {]l de tinta.'.format(tinta)
+print('Para pintar essa parede, você precisará de {}L de tinta.'.format(tinta))
