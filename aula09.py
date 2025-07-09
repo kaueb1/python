4e31fd6053607cb885e580949a60e38f3937436e
kaueb1 <kauebvieira@gmail.com>
2025-07-09 17:42:41 -0300

    Aula 9 – Manipulando Texto

---
+++ aula09.py
@@ -0,0 +1,9 @@
+frase = ' Curso em Vídeo Python'
+print(frase[::1])
+print(frase.upper().count('o'))
+print(len(frase))
+print(len(frase.strip()))
+print(frase.replace('Python', 'Android'))
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.split())
