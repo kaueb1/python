58d4d8a229f3394f76dfe1c1a4152dc854353d6e
kaueb1 <kauebvieira@gmail.com>
2025-08-14 15:42:45 -0300

    Exercício 50 – Soma dos pares

---
+++ ex050.py
@@ -0,0 +1,7 @@
+soma = 0
+cont = 0
+for c in range(1, 7):
+    num = int(input('Digite o {} valor: '.format(c)))
+    soma = soma + num
+    cont += 1
+print('Você informou {} números e a soma foi {}'.format(cont, soma))
