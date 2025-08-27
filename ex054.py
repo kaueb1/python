73606047bef11edcbf60be413c52661f66441641
kaueb1 <kauebvieira@gmail.com>
2025-08-27 15:24:07 -0300

    Exercício 56 – Analisador completo

--- ex054.py
+++ ex054.py
@@ -1,7 +1,7 @@
 from datetime import date
 atual = date.today().year
-nasc = int(input('Em que ano a pessoa nasceu'.))
+nasc = int(input('Em que ano a pessoa nasceu?' ))
 idade = atual - nasc
 if idade >= 21:
-    print('Essa pessoa é maior')
-else: print('Essa pessoa é menor')
+    print('Essa pessoa é de maior')
+else: print('Essa pessoa é de menor')
