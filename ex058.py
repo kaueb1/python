8e7ad868c41f63eed61ce32404363b77fb36c25a
kaueb1 <kauebvieira@gmail.com>
2025-09-03 15:48:07 -0300

    Exercício 58 – Jogo da Adivinhação v2.0

---
+++ ex058.py
@@ -0,0 +1,13 @@
+from random import randint
+computador = randint(0,10)
+print('Sou seu computador...')
+print('Acabei de pensar em um número de 0 e 10.')
+print('Será que você consegue adivinhar qual foi?')
+acertou = False
+palpites = 0
+while not acertou:
+    jogador = int(input('Qual é o seu palpite? '))
+    palpites += 1
+    if jogador == computador:
+        acertou = True
+print('Acertou com {} palpites'.format(palpites))
