4c9c67280312df371c908f777ec9fb0c161f1b56
kaueb1 <kauebvieira@gmail.com>
2025-09-08 16:22:10 -0300

    Exercício 62 – Super Progressão Aritmética v3.0

--- ex061.py
+++ ex061.py
@@ -5,7 +5,7 @@ razão = int(input('Razão da PA: '))
 termo = primeiro
 cont = 1
 while cont <= 10:
-    print('{} → '.format(termo))
+    print('{} → '.format(termo), end='')
     termo += razão
     cont += 1
 print('FIM')
