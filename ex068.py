fdede1dcc8d381798ac002ff9a98069e682b8342
kaueb1 <kauebvieira@gmail.com>
2025-09-17 14:08:11 -0300

    Exercício 68 – Jogo do Par ou Ímpar

---
+++ ex068.py
@@ -0,0 +1,26 @@
+from random import randint
+v = 0
+while True:
+    jogador = int(input('Diga um valor: '))
+    computador = randint(0, 10)
+    total = jogador + computador
+    tipo = ''
+    str(input('Par ou ímpar? '))
+    while tipo not in 'PI':
+        tipo = str(input('Par ou ímpar? [P/I] ')).strip().upper()[0]
+    print(f'Você jogou {jogador} e o computador {computador}. Total de {total}')
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu!')
+            v += 1
+        else:
+            print('Você perdeu!')
+            break
+    elif tipo == 'I':
+        if total % 2 == 1:
+            print('Você ganhou!')
+        else:
+            print('Você perdeu!')
+            break
+    print('Vamos jogar novamente...')
+print(f'Game over! Você venceu {v} vezes.')
