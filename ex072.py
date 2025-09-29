c57f9df8922daea96cc8d5910e57711b8bd5c50a
kaueb1 <kauebvieira@gmail.com>
2025-09-29 16:09:50 -0300

    Exercício Python #072 - Número por Extenso

---
+++ ex072.py
@@ -0,0 +1,10 @@
+cont = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis',
+        'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+        'treze', 'quatorze', 'quinze', 'desesseis', 'desessete',
+        'dezoito', 'dezenove', 'vinte')
+while True:
+    núm = int(input('Digite um número entre 0 e 20: '))
+    if 0 <= núm <= 20:
+        break
+    print('Tente novamente. ', end='')
+print(f'Você digitou o número {cont[núm]}')
